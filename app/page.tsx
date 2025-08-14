2835edb8406c1200831328802fb57470ff30daa9
jisun <kjisun1221@gmail.com>
2025-08-14 21:45:04 +0900

    💄 main 화면 UI 구성 ( Hero UI Components 생성 )

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Hero from "@/components/scene/Hero";
+
+export default function Home() {
+    return (
+        <main className="w-full h-screen flex items-center justify-center overflow-hidden text-white bg-[var(--color-background)]">
+            <Hero />
+        </main>
+    );
+}
