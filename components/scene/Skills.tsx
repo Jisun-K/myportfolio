a983327eb900052c1e32eb2e804b88e3a4fe6f70
jisun <kjisun1221@gmail.com>
2025-08-18 17:23:01 +0900

    💄 skill component ui ing

--- components/scene/Skills.tsx
+++ components/scene/Skills.tsx
@@ -1,7 +1,48 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
+
+const skills = [
+    {
+        name: "Angular2",
+        src: ""
+    },
+    {
+        name: "Next.js",
+        src: ""
+    },
+    {
+        name: "Typescript",
+        src: ""
+    },
+    {
+        name: "Javascript",
+        src: ""
+    },
+    {
+        name: "Firebase",
+        src: ""
+    },
+    {
+        name: "Github",
+        src: ""
+    },
+    {
+        name: "Java",
+        src: ""
+    }
+]
+
 export default function Skills() {
     return (
         <section className="flex flex-col justify-center items-center px-6 md:px-0 max-w-3xl mx-auto h-screen text-[var(--color-text)]">
-            스킬
+            {skills.map((skill) => {
+                return (
+                    <Avatar id={skill.name}>
+                        <AvatarImage src={skill.src} />
+                        <AvatarFallback>{skill.name}</AvatarFallback>
+                    </Avatar>
+                );
+            }
+            )}
         </section>
     )
 }
