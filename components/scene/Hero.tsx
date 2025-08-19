5fe595279971a74b810a3e7b73f14f634bc4d06c
jisun <kjisun1221@gmail.com>
2025-08-19 14:19:21 +0900

    💄 skill components UI 수정 ( 타입별 hover효과 )

--- components/scene/Hero.tsx
+++ components/scene/Hero.tsx
@@ -1,10 +1,10 @@
 export default function Hero() {
     return (
-        <section className="flex flex-col justify-center items-center px-6 md:px-0 max-w-3xl mx-auto h-screen text-[var(--color-text)]">
+        <section className="section-base">
             <h1 className="w-full md:w-auto text-3xl md:text-5xl font-bold leading-snug md:leading-tight ">
                 안녕하세요, <br />
                 프론트엔드 개발자 <br className="md:hidden" />
-                <em className="text-[var(--color-accent)] not-italic font-extrabold">김지선</em> 입니다.
+                <em className="text-accent not-italic font-extrabold">김지선</em> 입니다.
             </h1>
             <p className="mt-4 text-base md:text-lg leading-tight md:max-w-[80%]">
                 Angular 경험을 바탕으로 웹 프로젝트를 개발하였고, 최근엔 Next.js 기능을 학습하며 효율적이고 인터랙티브한 사용자 경험을 만드는 데 집중하고 있습니다.
--- components/scene/Skills.tsx
+++ components/scene/Skills.tsx
@@ -1,48 +1,109 @@
+"use client"
+
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
+import { useState } from "react"
+
+import AngularIcon from "/images/ic_angular.svg"
 
 const skills = [
     {
         name: "Angular2",
-        src: ""
+        src: "/images/ic_angular.svg",
+        level: 0.8,
+        type: "work"
     },
     {
         name: "Next.js",
-        src: ""
+        src: "/images/ic_next.svg",
+        level: 0.8,
+        type: "personal"
     },
     {
         name: "Typescript",
-        src: ""
+        src: "/images/ic_typescript.svg",
+        level: 0.8,
+        type: "work"
     },
     {
         name: "Javascript",
-        src: ""
+        src: "/images/ic_javascript.svg",
+        level: 0.8,
+        type: "personal"
+
+    },
+    {
+        name: "TailwindCSS",
+        src: "/images/ic_tailwind.svg",
+        level: 0.8,
+        type: "personal"
+
     },
     {
         name: "Firebase",
-        src: ""
-    },
-    {
-        name: "Github",
-        src: ""
+        src: "/images/ic_firebase.svg",
+        level: 0.8,
+        type: "work"
+
     },
     {
         name: "Java",
-        src: ""
+        src: "/images/ic_java.svg",
+        level: 0.8,
+        type: "personal"
+
+    },
+    {
+        name: "React",
+        src: "/images/ic_react.svg",
+        level: 0.8,
+        type: "personal"
+    },
+    {
+        name: "Github",
+        src: "/images/ic_github.svg",
+        level: 0.8,
+        type: "work"
     }
 ]
 
 export default function Skills() {
+    const [hoveredSkillType, setHoveredSkillType] = useState<string | null>(null)
+
     return (
-        <section className="flex flex-col justify-center items-center px-6 md:px-0 max-w-3xl mx-auto h-screen text-[var(--color-text)]">
-            {skills.map((skill) => {
-                return (
-                    <Avatar id={skill.name}>
-                        <AvatarImage src={skill.src} />
-                        <AvatarFallback>{skill.name}</AvatarFallback>
-                    </Avatar>
-                );
+        <section className="section-base">
+            {hoveredSkillType ?
+                <h3 className="mb-4 h-6 text-center font-xl text-background bg-accent">
+                    <span className="p-2">{hoveredSkillType.toUpperCase()}</span>
+                </h3>
+                : <h1 className="text-accent text-xl font-bold mb-4 h-6">기술 스택</h1>
             }
-            )}
-        </section>
+            {/* <h1 className="text-accent text-xl font-bold mb-4">기술 스택</h1>
+            <h3 className="mb-4 h-6 text-center font-medium text-background bg-accent">
+                {hoveredSkillType && <span className="p-2">{hoveredSkillType.toUpperCase()}</span>}
+            </h3> */}
+            <div className="grid grid-cols-3 gap-4 mt-[8px] ">
+                {skills.map((skill) => {
+                    const isSameType = hoveredSkillType === skill.type;
+                    const isOtherType = hoveredSkillType && hoveredSkillType !== skill.type;
+
+                    return (
+                        <div key={skill.name} className={`w-24 rounded-xl border border-gray-200 bg-white shadow-sm p-4 transition-transform duration-200 
+                ${isSameType ? "shadow-md scale-[1.1]" : ""}
+                ${isOtherType ? "blur-[1px] opacity-90" : ""}`}
+                            onMouseEnter={() => setHoveredSkillType(skill.type)}
+                            onMouseLeave={() => setHoveredSkillType(null)}>
+                            {/* <div
+                                className="absolute bottom-0 left-0 w-full bg-blue-400 opacity-30 rounded-b-xl transition-all duration-500"
+                                style={{ height: `${skill.level * 100}%` }}
+                            /> */}
+                            <Avatar>
+                                <AvatarImage src={skill.src} alt={skill.name} />
+                                <AvatarFallback>{skill.name}</AvatarFallback>
+                            </Avatar>
+                        </div>
+                    )
+                })}
+            </div>
+        </section >
     )
 }
