2c364654a40da3041475646a39dd6e50d631dd02
jisun <kjisun1221@gmail.com>
2025-08-20 12:52:35 +0900

    💄 global.css 색상 수정

--- components/scene/Hero.tsx
+++ components/scene/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
                 <em className="text-accent not-italic font-extrabold">김지선</em> 입니다.
             </h1>
             <p className="mt-4 text-base md:text-lg leading-tight md:max-w-[80%]">
-                Angular 경험을 바탕으로 웹 프로젝트를 개발하였고, 최근엔 Next.js 기능을 학습하며 효율적이고 인터랙티브한 사용자 경험을 만드는 데 집중하고 있습니다.
+                Angular 경험을 바탕으로 웹 프로젝트를 개발하였고, 최근엔 Next.js  활용하여 효율적이고 인터랙티브한 사용자 경험을 만드는 데 집중하고 있습니다.
             </p>
         </section>
     );
