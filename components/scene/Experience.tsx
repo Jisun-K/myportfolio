294ac3c373a432e671301a40263f297d0484d6ab
jisun <kjisun1221@gmail.com>
2025-08-21 15:18:39 +0900

    🐛 CSS 수정으로 섹션 영역 벗어남 문제 해결(임시)

--- components/scene/Experience.tsx
+++ components/scene/Experience.tsx
@@ -5,7 +5,8 @@ export default function Experience() {
     return (
         <section className="section-base md:max-w-5xl">
             <h1 className="section-title">경력 사항</h1>
-            <p className="text-accent text-2xl font-ligth break-all text-center md:w-auto">다양한 프로젝트를 통해 실무 경험과 기술 역량을 꾸준히 쌓아가고 있습니다.</p>
+            <p className="text-accent text-2xl font-ligth break-all text-center md:w-auto">
+                다양한 프로젝트를 통해 실무 경험과<br className="hidden md:block"/> 기술 역량을 꾸준히 쌓아가고 있습니다.</p>
             <div className="md:flex md:justify-center md:items-start mt-6">
                 <ExperienceList data={experienceList}/>
             </div>
